47e46f84ea0973e73b42970b6a3cc5ce3e212606
Nathan4043 <nathanr88@nycstudents.net>
2025-12-23 09:36:00 -0500

    Update Work.py

---
+++ Work.py
@@ -0,0 +1,9 @@
+class zombie():
+    def __init__(self,name,drops,health,power):
+        self.name = name
+        self.drops = drops
+        self.health = int(health)
+        self.health = int(power)
+
+zombie1 =("zombie",[], 40, 1)
+print(zombie1)
