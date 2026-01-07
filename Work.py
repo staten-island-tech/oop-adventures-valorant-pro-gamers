2ae3913bc91109e94c89ca7f45a8bb3ec1ec6fcb
Nathan4043 <nathanr88@nycstudents.net>
2026-01-07 10:16:49 -0500

    Update Work.py

--- Work.py
+++ Work.py
@@ -1,8 +1,7 @@
-   
+import random
 name=input("name pet")
 class pet():
-    def __init__(self,name,health,food,hunger,thirst,power,coins,defence,items):
-        self.items(items)
+    def __init__(self,name,health,food,hunger,thirst,power,coins,defence):
         self.name=name
         self.health=int(health)
         self.food=int(food)
@@ -11,8 +10,7 @@ class pet():
         self.power=int(power)
         self.coins=int(coins)
         self.defence=int(defence)
-        self.items=["junk"]
-self=pet(name,100,2,75,75,2,10)
+self=pet(name,100,2,75,75,2,100000,5)
 print(self.__dict__)
 while True:
     action=input("Fight or Feed?")
@@ -36,9 +34,16 @@ while True:
             
     def fight():
         print("fought")
+    def slots():
+        slot=random.randint(7,77)
+        self.coins -=5
+        if slot == 77:
+            print("YOU WON BIG KEEP GOING DUDE")
+            self.money += 777
     options = {
         "fight":fight,
-        "feed":feed
+        "feed":feed,
+        "slots":slots
                 }
     choice = action.lower()
     if choice in options:
